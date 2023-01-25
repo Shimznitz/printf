afc8b8e353e83eaa9d069901cfec53c0c49d02f0
Daniel Siman <siman.aken@gmail.com>
2023-01-25 04:04:21 -0800

    prints what ever

---
+++ _printf.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <string.h>
+#include "main.h"
+/**
+ * _printf - prints anything
+ * @format: the format string
+ *
+ * Return: number of bytes printed
+ */
+int _printf(const char *format, ...)
+{
+	int _printf(const char *format, ...)
+	{
+		printf(format);
+
+		return (strlen(format));
+	}
+}
